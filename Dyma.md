55ce1c82763079c6d5324d8223994a030180e1df
Brice <fbrice57@gmail.com>
2024-03-04 15:16:34 +0100

    gitignore

---
+++ Dyma.md
@@ -0,0 +1,5 @@
+React
+
+Composé de composants, élément root, créé un DOM virtuel rapide pour travailler et change le DOM HTML en étant performant
+
+JSX permet de créer des éléments react rapidement
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// eslint-disable-next-line react-refresh/only-export-components
+// const ReactElement = React.createElement('h1', { className: 'my-class'}, 'Hello world');
+
+const h1 = <h1 className='salut'>Hello world !</h1>
+
+// const tree = React.createElement('div', { className: 'my-div'}, ReactElement);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {h1}
+
+    
+    {/* <App /> */}
   </React.StrictMode>,
 )
